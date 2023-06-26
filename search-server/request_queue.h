345a1f1314d6bb0d8340620c95edc0e6a027f594
Lesha <jeinerox@gmail.com>
2023-06-26 04:52:52 +0300

    multiple files 0.2

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -10,11 +10,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server) : search_server_(search_server) {}
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        addResult(raw_query, result);
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
     int GetNoResultRequests() const;
@@ -28,3 +24,10 @@ private:
     const static int min_in_day_ = 1440;
     void addResult(const std::string& query, const std::vector<Document>& result);
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    addResult(raw_query, result);
+    return result;
+}
